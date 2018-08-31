5a3c76ece0525205c808b16cd29b17269b233cc1
William Gonzalez <wifagoco@gmail.com>
2018-08-30 19:35:00 -0500

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  severElements = [];
+  newServerName = "";
+  newServerContent = "";
+
+  onAddServer(){
+   this.severElements.push({
+     type:'server',
+     name:this.newServerName,
+     content:this.newServerContent
+   });
+  }
+
+  onAddblueprint(){
+    this.severElements.push({
+      type:'blueprint',
+      name:this.newServerName,
+      content:this.newServerContent
+    });
+  }
+
+
 }
+
