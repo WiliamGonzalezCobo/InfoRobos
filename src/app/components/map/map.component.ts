2d4a2a3ce3211e2e54c87158ad07208274236dba
William Gonzalez <wifagoco@gmail.com>
2018-10-04 19:34:02 -0500

    se realiza implementacion de lectura de localizacion del navegador

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -9,8 +9,14 @@ import { } from '@types/googlemaps';
 export class MapComponent {
   @ViewChild('gmap') gmapElement: any;
   map: google.maps.Map;
+  isTracking : boolean;
+  currentLat : string;
+  currentLong : string;
+  marker : google.maps.Marker;
 
   ngOnInit() {
+
+
     var mapProp = {
       center: new google.maps.LatLng(18.5793, 73.8143),
       zoom: 15,
@@ -18,9 +24,71 @@ export class MapComponent {
     };
 
     this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+    this.findMe();
+
   }
 
   setMapType(mapTypeId: string) {
     this.map.setMapTypeId(mapTypeId)
+  }
+
+  findMe() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.showPosition(position);
+      });
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+
+  trackMe() {
+    if (navigator.geolocation) {
+      this.isTracking = true;
+      navigator.geolocation.watchPosition((position) => {
+        this.showTrackingPosition(position);
+      });
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+
+  showPosition(position) {
+    this.currentLat = position.coords.latitude;
+    this.currentLong = position.coords.longitude ;
+
+    let location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    this.map.panTo(location);
+
+    if (!this.marker) {
+      this.marker = new google.maps.Marker({
+        position: location,
+        map: this.map,
+        title: 'Got you!'
+      });
+    }
+    else {
+      this.marker.setPosition(location);
+    }
+  }
+
+  showTrackingPosition(position) {
+    console.log(`tracking postion:  ${position.coords.latitude} - ${position.coords.longitude}`);
+    this.currentLat = position.coords.latitude;
+    this.currentLong = position.coords.longitude;
+
+    let location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    this.map.panTo(location);
+
+    if (!this.marker) {
+      this.marker = new google.maps.Marker({
+        position: location,
+        map: this.map,
+        title: 'Got you!'
+      });
+    }
+    else {
+      this.marker.setPosition(location);
+    }
   }
 }
